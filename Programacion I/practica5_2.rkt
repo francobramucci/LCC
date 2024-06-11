7a45a74d30cad2f5f167e8f78aae156887e254fa
francobramucci <bramuccifranco1@gmail.com>
2024-06-10 22:54:49 -0300

    Practica 5_2 terminada

--- Programacion I/practica5_2.rkt
+++ Programacion I/practica5_2.rkt
@@ -50,7 +50,7 @@
   (filter positive? l)
   )
 
-; deleteocurr :: List(Number) -> Number -> List(Number)
+; deleteocurr :: List(Number), Number -> List(Number)
 ; occur :: Number -> Bool
 (define (deleteoccur l n)
   (local((define (occur x) (not (= x n))))
@@ -58,12 +58,12 @@
     )
   )
 
-; deleteocur2 :: List(Number) -> Number -> List(Number)
+; deleteocur2 :: List(Number), Number -> List(Number)
 (define (deleteoccur2 l n)
     (filter (lambda (x) (not(= x n)) ) l)
   )
 
-; deleteocurr :: List(Number) -> Number -> List(Number)
+; eliminar :: List(Number) -> List(Number)
 (define (eliminar l)
   (local (
           ; distinto? : Number -> Boolean
@@ -138,6 +138,236 @@
 (define (convertirFC l)
   (map FtoC l)
   )
+
+; Ejercicio 13
+
+; prod :: List(Number) -> Number
+(define (prod l) (foldr * 1 l))
+
+; Ejercicio 14
+
+; pegar :: List(String) -> String
+(define (pegar l) (foldr string-append "" l))
+
+; Ejercicio 15
+
+; max1 :: List(Number) -> Number
+(define (max1 l) (foldr max 0 l))
+
+; Ejercicio 16
+
+; and1 :: Boolean, Boolean -> Boolean
+(define (and1 x y) (and x y))
+
+; todos-verdaderos :: List(Boolean) -> Boolean
+(define (todos-verdaderos l) (foldr and1 #t l))
+
+; Ejercicio 17
+
+; lar :: Any, Number -> Number
+(define (lar x y) (+ 1 y))
+
+; largo :: List(Any) -> Number
+(define (largo l) (foldr lar 0 l))
+
+; Ejercicio 18
+
+; mayores :: List(Number), Number -> List(Number)
+(define (mayores l m)
+  (local (
+          (define (mayor n) (> n m))
+          )
+    (filter mayor l)
+    )
+  )
+
+; Ejercicio 19
+
+; largas :: List(String), Number -> List(String)
+(define (largas l m)
+  (local (
+          (define (gtm s) (> (string-length s) m))
+          )
+    (filter gtm l)
+    )
+  )
+
+; Ejercicio 20
+
+; lejos  :: List(posn), Number -> List(posn)
+(define (lejos l m)
+  (local (
+          (define (distgtm p) (> (sqrt (+ (sqr (posn-x p)) (sqr (posn-y p)))) m))
+          )
+    (filter distgtm l)
+    )
+  )
+
+; Ejercicio 21
+
+; sumar :: List(Number), Number -> List(Number)
+(define (sumar l m)
+  (local (
+          (define (suma x) (+ x m))
+          )
+    (map suma l)
+    )
+  )
+
+; Ejercicio 22
+
+; elevar :: List(Number), Number -> List(Number)
+(define (elevar l m)
+  (local (
+          (define (raise x) (expt x m))
+          )
+    (map raise l)
+    )
+  )
+
+; Ejercicio 23
+
+; sumcuad :: List(Number) -> Number
+(define (sumcuad l)
+  (foldr + 0 (map sqr l))
+  )
+
+; Ejercicio 24
+
+; sumdist :: List(posn) -> Number
+(define (sumdist l)
+  (foldr + 0 (map distancia l))
+  )
+
+; Ejercicio 25
+
+; multpos :: List(Number) -> Number
+(define (multpos l)
+  (foldr * 1 (filter positive? l))
+  )
+
+; Ejercicio 26
+
+; sumabs :: List(Number) -> Number
+(define (sumabs l)
+  (foldr + 0 (map abs l))
+  )
+
+; Ejercicio 27
+
+(define (raices1 l)
+  (map sqrt (filter positive? l))
+  )
+
+; Ejercicio 28
+
+; ancha? :: Image -> Boolean
+(define (ancha? i) (> (image-width i) (image-height i)))
+
+; area :: Image -> Number
+(define (area i) (* (image-width i) (image-height i)))
+
+; saa :: List(Image) -> Number
+(define (saa l)
+  (foldr + 0 (map area (filter ancha? l)))
+  )
+
+; Ejercicio 29
+
+; sumalistpos? :: List(Number) -> Boolean
+(define (sumalistpos? l)
+  (> (foldr + 0 l) 0)
+  )
+
+; or1 :: Boolean, Boolean -> Boolean
+(define (or1 x y) (or x y))
+
+; algun-pos :: List(List(Number)) -> Boolean
+(define (algun-pos l)
+  (foldr or1 #f (map sumalistpos? l))
+  )
+
+; Ejercicio 30
+
+; gt4? :: List(Any) -> Boolean 
+(define (gt4? l) (> (length l) 4))
+
+; long-lists :: List(List(Any)) -> Boolean
+(define (long-lists l)
+  (foldr and1 #t (map gt4? l))
+  )
+
+; Ejercicio 31
+(define (todos-true l)
+  (foldr and1 #t (filter boolean? l))
+  )
+
+; Ejercicio 32
+
+(define-struct alumno [nombre nota faltas])
+
+
+; notageq9 :: alumno -> Boolean
+(define (notageq9 a) (>= (alumno-nota a) 9))
+; destacados :: List(alumno) -> List(alumno)
+(define (destacados l)
+  (filter notageq9 l)
+  )
+
+
+; condic :: alumno -> String
+(define (condic a)
+  (cond [(>= (alumno-nota a) 8) "Promovido"]
+        [(< (alumno-nota a) 6) "Libre"]
+        [else "Regular"]
+        )
+  )
+; condicion :: List(alumno) -> List(String)
+(define (condicion l)
+  (map condic l)
+  )
+
+
+; verlib :: String, Boolean -> Boolean
+(define (verlib x y)
+  (and (not(string=? x "Libre")) y)
+  )
+; exito :: List(alumno) -> Boolean
+(define (exito l)
+  (foldr verlib #t (condicion l)) 
+  )
+
+
+; regular? :: alumno -> Boolean
+(define (regular? a)
+  (string=? (condic a) "Regular")
+  )
+; sumaus :: alumno, Number -> Number
+(define (sumaus x y)
+  (+ (alumno-faltas x) y)
+  )
+; faltas-regulares :: List(alumno) -> Number
+(define (faltas-regulares l)
+  (foldr sumaus 0 (filter regular? l))
+  )
+
+
+; promaus3 :: alumno -> Boolean
+(define (promaus3 a)
+  (and (string=? (condic a) "Promovido") (>= (alumno-faltas a) 3))
+  )
+; promovidos-ausentes :: List(alumno) -> List(String)
+(define (promovidos-ausentes l)
+  (map alumno-nombre (filter promaus3 l))
+  )
+
+
+
+
+
+
+
+
 
 
 
