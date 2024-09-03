5f53a1151a6a6f8f38a4d305fb31ae30c7d95ee5
facultad <facultad@facu.com>
2024-09-03 10:00:09 -0300

    Ejs práctica 3

--- Programacion_II/Python/practica3.py
+++ Programacion_II/Python/practica3.py
@@ -42,3 +42,81 @@ def ordenada(l):
             return False
 
 #Lucas, dado dos número iniciales y un índice, devolver el valor de la secuencia en dicho índice 
+
+# Toma 3 numeros y hace una sucesion de fibonacci iniciando con los primeros 2 numeros
+#y retorna el numero n del la sucesion
+
+def lucasFL(f1 , f2 , n):
+    l = [f1,f2]
+    for i in range(n):
+        l.append(l[-2] + l[-1])
+    return l
+
+print(lucasFL(2,1,9))
+
+# Falta hacer lucasF que retorna el valor en n y no la lista completa
+
+def lucasR(f1 , f2 , n):
+    if n > 0:
+        return lucasR(f2 , f1+f2 , n-1)
+    else:
+        return f2
+
+print(lucasR(2,1,9))
+
+#5
+def duplicado(l):
+    aux = sorted(l)
+    for i in range(len(aux)-2):
+        if aux[i] == aux[i+1]:
+            return True
+    return False
+
+#6
+def eliminaDuplicados(l):
+    l.sort()
+    temp = l[0]
+    res = []
+    for i in range(len(l)):
+        if temp != l[i]:
+            res.append(temp)
+            temp = l[i]
+    res.append(temp)
+    return res
+
+#7
+def contdist(l):
+    return len(eliminaDuplicados(l))
+
+#8
+def BusquedaDicotomica(l,s):
+    l.sort()
+    ini = 0
+    end = len(l)-1
+    mid = (end+ini)//2
+    while ini != end:
+        if s == l[mid]:
+            return True
+        if s > l[mid]:
+            ini = mid
+            mid = (end+ini)//2
+        if s < l[mid]:
+            end = mid
+            mid = (end+ini)//2
+            
+
+            
+def diablo(l):
+    for i in range(-1,-len(l)-1,-1):
+        print(l[i])
+         
+
+
+
+        
+    
+
+
+
+
+
