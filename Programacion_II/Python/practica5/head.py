cc141139e0a79a1e8a741f2b4c4526d94e037cd1
francobramucci <francohokage7@gmail.com>
2024-10-03 15:15:53 -0300

    Corrección argumentos del sistema

--- Programacion_II/Python/practica5/head.py
+++ Programacion_II/Python/practica5/head.py
@@ -1,13 +1,15 @@
 import sys
 
 def main(s, n):
+    n = int(n)
     f = open(s, 'r')
     for i in range(n):
-        f.readline()
+        print(f.readline())
 
 if __name__ == "__main__":
-    if(len(sys.argv) != 2):
+    if(len(sys.argv) != 3):
+        print(sys.argv[0], sys.argv[1], sys.argv[2])
         print("Ingrese la ruta de un archivo y la cantidad de lineas a leer.")
         exit()
     else:
-        main(sys.argv[0], sys.argv[1])
+        main(sys.argv[1], sys.argv[2])
