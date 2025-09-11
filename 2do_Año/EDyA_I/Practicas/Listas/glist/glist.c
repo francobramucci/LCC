bcc74022d5aeecb9e4ad737b9f18d9967f392b32
francobramucci <bramuccifranco1@gmail.com>
2025-09-11 17:54:39 -0300

    Elimino archivos redundantes

--- 2do_Año/EDyA_I/Practicas/Listas/glist/glist.c
+++ 2do_Año/EDyA_I/Practicas/Listas/glist/glist.c
@@ -4,7 +4,7 @@
  * Devuelve una lista vacía.
  */
 GList glist_crear() {
-	return NULL;
+    return NULL;
 }
 
 /**
@@ -12,20 +12,20 @@ GList glist_crear() {
  * destroy es una función que libera el dato almacenado.
  */
 void glist_destruir(GList list, FuncionDestructora destroy) {
-	GNode *nodeToDelete;
-	while (list != NULL) {
-		nodeToDelete = list;
-		list = list->next;
-		destroy(nodeToDelete->data);
-		free(nodeToDelete);
-	}
+    GNode *nodeToDelete;
+    while (list != NULL) {
+        nodeToDelete = list;
+        list = list->next;
+        destroy(nodeToDelete->data);
+        free(nodeToDelete);
+    }
 }
 
 /**
  * Determina si la lista es vacía.
  */
 int glist_vacia(GList list) {
-	return (list == NULL);
+    return (list == NULL);
 }
 
 /**
@@ -33,37 +33,35 @@ int glist_vacia(GList list) {
  * copy es una función que retorna una copia física del dato.
  */
 GList glist_agregar_inicio(GList list, void *data, FuncionCopia copy) {
-	GNode *newNode = malloc(sizeof(GNode));
-	assert(newNode != NULL);
-	newNode->next = list;
-	newNode->data = copy(data);
-	return newNode;
+    GNode *newNode = malloc(sizeof(GNode));
+    assert(newNode != NULL);
+    newNode->next = list;
+    newNode->data = copy(data);
+    return newNode;
 }
 
 /**
  * Recorrido de la lista, utilizando la funcion pasada.
  */
 void glist_recorrer(GList list, FuncionVisitante visit) {
-	for (GNode *node = list; node != NULL; node = node->next)
-		visit(node->data);
+    for (GNode *node = list; node != NULL; node = node->next)
+        visit(node->data);
 }
 
 /**
  * Toma una lista y un predicado, devuelve una lista con los elementos que
  * evaluen a verdadero.
  */
-GList glist_filtrar(GList lista, FuncionCopia copy, Predicado predicado){
-	GList nuevaLista = glist_crear();
-	if(glist_vacia(lista))
-		return nuevaLista;
-	if(predicado(lista->data)){
-		nuevaLista = glist_agregar_inicio(nuevaLista, lista->data, copy);
-		nuevaLista->next = glist_filtrar(lista->next, copy, predicado);
-	}
-	else{
-		nuevaLista = glist_filtrar(lista->next, copy, predicado);
-	}
+GList glist_filtrar(GList lista, FuncionCopia copy, Predicado predicado) {
+    GList nuevaLista = glist_crear();
+    if (glist_vacia(lista))
+        return nuevaLista;
+    if (predicado(lista->data)) {
+        nuevaLista = glist_agregar_inicio(nuevaLista, lista->data, copy);
+        nuevaLista->next = glist_filtrar(lista->next, copy, predicado);
+    } else {
+        nuevaLista = glist_filtrar(lista->next, copy, predicado);
+    }
 
-	return nuevaLista;
+    return nuevaLista;
 }
-
--- 2do_Año/EDyA_I/Practicas/Listas/slist/dlist.c
+++ 2do_Año/EDyA_I/Practicas/Listas/slist/dlist.c
@@ -1,41 +1,36 @@
 #include "dlist.h"
 
-DList* dlist_crear(){
-	DList *lista = malloc(sizeof(DList));
-	lista->primero = NULL;
-	lista->ultimo = NULL;
+DList *dlist_crear() {
+    DList *lista = malloc(sizeof(DList));
+    lista->primero = NULL;
+    lista->ultimo = NULL;
 
-	return lista;
+    return lista;
 }
 
-void dlist_agregar_inicio(DList* lista, int dato){
-	DNodo* nuevoNodo = malloc(sizeof(DNodo));
-	nuevoNodo->dato = dato;
-	nuevoNodo->sig = lista->primero;
-	nuevoNodo->ant = NULL;
+void dlist_agregar_inicio(DList *lista, int dato) {
+    DNodo *nuevoNodo = malloc(sizeof(DNodo));
+    nuevoNodo->dato = dato;
+    nuevoNodo->sig = lista->primero;
+    nuevoNodo->ant = NULL;
 
-	if(lista->primero == NULL){
-		lista->primero = nuevoNodo;
-	}
-	else{
-		lista->primero->ant = nuevoNodo;
-		lista->primero = nuevoNodo;
-	}
+    if (lista->primero == NULL) {
+        lista->primero = nuevoNodo;
+    } else {
+        lista->primero->ant = nuevoNodo;
+        lista->primero = nuevoNodo;
+    }
 
-	if(lista->ultimo == NULL)
-		lista->ultimo = nuevoNodo;
+    if (lista->ultimo == NULL)
+        lista->ultimo = nuevoNodo;
 }
 
-void dlist_recorrer(DList *lista, DListOrdenDeRecorrido orden, void(*funcion)(int)){
-	if(!orden){
-		for(DNodo* temp = lista->primero; temp != NULL; temp = temp->sig)
-			funcion(temp->dato);
-	}
-	else{
-		for(DNodo* temp = lista->ultimo; temp != NULL; temp = temp->ant)
-			funcion(temp->dato);
-	}
+void dlist_recorrer(DList *lista, DListOrdenDeRecorrido orden, void (*funcion)(int)) {
+    if (!orden) {
+        for (DNodo *temp = lista->primero; temp != NULL; temp = temp->sig)
+            funcion(temp->dato);
+    } else {
+        for (DNodo *temp = lista->ultimo; temp != NULL; temp = temp->ant)
+            funcion(temp->dato);
+    }
 }
-
-
-
