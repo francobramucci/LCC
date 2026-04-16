cde24c6c47ca4261e7471881286d3b9c31e55936
francobramucci <bramuccifranco1@gmail.com>
2026-04-16 17:29:57 -0300

    Corrijo función esBisiesto

--- 3er_Año/EDyA_II/Lab1.hs
+++ 3er_Año/EDyA_II/Lab1.hs
@@ -113,7 +113,7 @@ Espaola, 22ª ed.)
 -}
 
 esBisiesto :: Int -> Bool
-esBisiesto x = ((mod x 4) == 0) && not (((mod x 100) == 0) && ((mod x 400) == 0))
+esBisiesto x = ((mod x 4) == 0) && (mod x 400) /= 0)
 
 {-
 4)
