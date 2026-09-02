cf87013e614159ba1243d870c09931fe74d36dea
francobramucci <bramuccifranco1@gmail.com>
2026-09-02 20:14:12 -0300

    ALP - Agrego parser para expresiones booleanas

--- 3er_Año/ALP/TP1/src/Parser.hs
+++ 3er_Año/ALP/TP1/src/Parser.hs
@@ -67,17 +67,47 @@ mulop = (reservedOp lis "*" >> return Times)
 intfactor :: Parser (Exp Int)
 intfactor = (parens lis intexp)
     <|>     do {n <- natural lis; return (Const (fromIntegral n))}
-    <|>     do {varid <- identifier lis; return (Var varid)}
     <|>     do {reservedOp lis "-"; iexp <- intexp; return (UMinus iexp)}
-    <|>     do {reservedOp lis "++"; varid <- identifier lis; return (VarInc varid)}
-    <|>     do {reservedOp lis "--"; varid <- identifier lis; return (VarDec varid)}
+    <|>     do {varid <- identifier lis;
+                do {reservedOp lis "++"; return (VarInc varid)}
+                <|> 
+                do {reservedOp lis "--"; return (VarDec varid)}
+                <|>
+                return (Var varid)
+               }
+
 
 ------------------------------------
 --- Parser de expresiones booleanas
 ------------------------------------
 
 boolexp :: Parser (Exp Bool)
-boolexp = undefined
+boolexp = chainl1 boolatom boolop
+
+boolop :: Parser (Exp Bool -> Exp Bool -> Exp Bool)
+boolop = (reservedOp lis "&&" >> return And)
+    <|>  (reservedOp lis "||" >> return Or)
+
+
+-- Intenta leer "==", reservedOp no consume la entrada si falla, entonces <|>
+-- ejecuta el siguiente parser. Si alguno lee correctamente el op entonces
+-- reservedOp si consume entrada, luego si se falla al leer la exp2 el <|> no
+-- intenta probar otro parser.
+
+boolatom :: Parser (Exp Bool)
+boolatom = (parens lis boolexp)
+    <|>    (reserved lis "true" >> return BTrue)
+    <|>    (reserved lis "false" >> return BFalse)
+    <|>    do {reservedOp lis "!"; e <- boolexp; return (Not e)} 
+    <|>    do {e1 <- intexp; 
+               do {reservedOp lis "=="; e2 <- intexp; return (Eq e1 e2)}
+               <|>
+               do {reservedOp lis "!="; e2 <- intexp; return (NEq e1 e2)}
+               <|>
+               do {reservedOp lis "<"; e2 <- intexp; return (Lt e1 e2)}
+               <|>
+               do {reservedOp lis ">"; e2 <- intexp; return (Gt e1 e2)}
+              }
 
 -----------------------------------
 --- Parser de comandos
